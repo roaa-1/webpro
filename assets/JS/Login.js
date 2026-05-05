95cc6e86d98d1c67ab2864f32c88d40cfc5f1a4f
Roaa <231110@ppu.edu.ps>
2026-05-05 18:54:27 +0300

    edit student dashboard

--- assets/JS/Login.js
+++ assets/JS/Login.js
@@ -7,40 +7,33 @@ const users = [
   { email: "admin@test.com", password: "1234", role: "admin" }
 ];
 
-// 🔥 لما يضغط زر تسجيل الدخول
 loginBtn.addEventListener("click", function () {
 
   const email = emailInput.value.trim();
   const password = passwordInput.value.trim();
 
-  // reset اللون
   emailInput.classList.remove("error");
   passwordInput.classList.remove("error");
 
-  // إذا فاضي
   if (!email || !password) {
     if (!email) emailInput.classList.add("error");
     if (!password) passwordInput.classList.add("error");
     return;
   }
 
-  // البحث عن المستخدم
   const user = users.find(
     u => u.email === email && u.password === password
   );
 
-  // إذا غلط
   if (!user) {
     emailInput.classList.add("error");
     passwordInput.classList.add("error");
     return;
   }
 
-  // تخزين
   localStorage.setItem("userRole", user.role);
   localStorage.setItem("userEmail", user.email);
 
-  // 🔥 التحويل حسب الدور
   if (user.role === "admin") {
     window.location.href = "AdministratorDashboard.html";
   } else {
@@ -49,7 +42,6 @@ loginBtn.addEventListener("click", function () {
 
 });
 
-// 🔥 يشيل اللون الأحمر أول ما يكتب
 [emailInput, passwordInput].forEach(input => {
   input.addEventListener("input", () => {
     input.classList.remove("error");
